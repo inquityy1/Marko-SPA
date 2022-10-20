6bdc1d0bda7186c714975411bc845b85269168f1
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-10-20 17:58:11 +0200

    full site

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import "./Header.css";
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div className="header">
+      <div className="links">
+        <Link to="/">Home</Link>
+      </div>
+      <div className="links">
+        <Link to="/second-page">First Page</Link>
+      </div>
+      <div className="links">
+        <Link to="/third-page">Second Page</Link>
+      </div>
+      <div className="links">
+        <Link to="/fourth-page">Third Page</Link>
+      </div>
+    </div>
+  );
+}
